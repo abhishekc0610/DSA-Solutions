6012115380a7216f9db6ad6d27be290f4630a4b2
abhishekc0610 <92425807+abhishekc0610@users.noreply.github.com>
2023-09-10 00:54:39 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (60.81%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -4,19 +4,17 @@ public:
         int n = pattern.size(), m = s.size();
         unordered_map<int, string> mp;
         
-        unordered_set<char> a;
-        unordered_set<string> b;
+        unordered_set<string> st;
         
         int j = 0, i = 0;
         for(i = 0; i<n && j<m; i++){
-            a.insert(pattern[i]);
             string x = "";
             
             while(j<m && s[j] != ' '){
                 x += s[j];
                 j++;
             }
-            b.insert(x);
+            st.insert(x);
             j++;
             
             if((mp.find(pattern[i]) != mp.end()) && (mp[pattern[i]] != x)) {
@@ -30,7 +28,7 @@ public:
         if(i<n || j<m) return false;
         
         // "abba" & "dog dog dog dog" -> false
-        if(a.size() != b.size()) return false;        
+        if(mp.size() != st.size()) return false;        
         
         return true;
     }
