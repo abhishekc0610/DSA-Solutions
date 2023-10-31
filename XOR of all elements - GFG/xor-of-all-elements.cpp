ce2d8065c44ebb1d0943b177e6efbee9d79e5c4c
abhishekc0610 <92425807+abhishekc0610@users.noreply.github.com>
2023-11-01 04:21:35 +0530

    Added solution - LeetHub

---
+++ XOR of all elements - GFG/xor-of-all-elements.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    vector<int> getXor(vector<int> A, int N)
+    {
+        // Write your Code here
+        int cnt = 0;
+        for(auto x : A) {
+            cnt = cnt ^ x;
+        }
+        
+        for(int i = 0; i < N; i++) {
+            A[i] = cnt ^ A[i];
+        }
+        
+        return A;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin >> N ;
+        vector<int> A(N);
+
+        for (int i = 0; i < N; i++)
+        {
+            cin >> A[i];
+        }
+
+        Solution ob;
+        vector<int> B = ob.getXor(A, N);
+        for(int i = 0 ; i < N ; i++)
+        {
+            cout << B[i] <<" ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
